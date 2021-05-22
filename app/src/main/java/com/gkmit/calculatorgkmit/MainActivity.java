2fffe087d6a90559075a8de1e78cf0421793bfe0
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-05-23 01:04:08 +0530

    added validation

--- app/src/main/java/com/gkmit/calculatorgkmit/MainActivity.java
+++ app/src/main/java/com/gkmit/calculatorgkmit/MainActivity.java
@@ -11,8 +11,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-    private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btn_add,btn_sub,btn_div,btn_ac,btn_mul,btn_eql;
-    private TextView result;
+    private Button button1,button2,button3,button4,button5,button6,button7,button8,button9,button0,buttonAdd,buttonSubtract,buttonDivide,buttonAllClear,buttonMultiply,buttonEqual;
+    private TextView tvResult;
     private float number1,number2;
     private boolean add,division,multiplication,subtraction;
 
@@ -20,68 +20,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        buttonAdd = (Button) findViewById(R.id.button_add);
+        buttonSubtract = (Button) findViewById(R.id.button_subtract);
+        buttonDivide = (Button) findViewById(R.id.button_divide);
+        buttonMultiply = (Button) findViewById(R.id.button_multiply);
+        buttonAllClear = (Button) findViewById(R.id.button_ac);
+        buttonEqual = (Button) findViewById(R.id.button_equal);
 
-        btn0 = (Button) findViewById(R.id.btn0);
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn4 = (Button) findViewById(R.id.btn4);
-        btn5 = (Button) findViewById(R.id.btn5);
-        btn6 = (Button) findViewById(R.id.btn6);
-        btn7 = (Button) findViewById(R.id.btn7);
-        btn8 = (Button) findViewById(R.id.btn8);
-        btn9 = (Button) findViewById(R.id.btn9);
-        btn_add = (Button) findViewById(R.id.btn_add);
-        btn_sub = (Button) findViewById(R.id.btn_sub);
-        btn_div = (Button) findViewById(R.id.btn_div);
-        btn_mul = (Button) findViewById(R.id.btn_mul);
-        btn_ac = (Button) findViewById(R.id.btn_ac);
-        btn_eql = (Button) findViewById(R.id.btn_eql);
+        tvResult = (TextView) findViewById(R.id.result_view);
 
-        result = (TextView) findViewById(R.id.result_view);
-
-        btn0.setOnClickListener(this);
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
-        btn6.setOnClickListener(this);
-        btn7.setOnClickListener(this);
-        btn8.setOnClickListener(this);
-        btn9.setOnClickListener(this);
-        btn_ac.setOnClickListener(this);
-        btn_add.setOnClickListener(this);
-        btn_sub.setOnClickListener(this);
-        btn_mul.setOnClickListener(this);
-        btn_div.setOnClickListener(this);
-        btn_eql.setOnClickListener(this);
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        buttonAllClear.setOnClickListener(this);
+        buttonAdd.setOnClickListener(this);
+        buttonSubtract.setOnClickListener(this);
+        buttonMultiply.setOnClickListener(this);
+        buttonDivide.setOnClickListener(this);
+        buttonEqual.setOnClickListener(this);
 
     }
 
     //function to perform operation
     private void compute(float number1, float number2) {
+
         if(add){
-            float answer = number1+number2;
-            result.setText(answer+"");
-            Toast.makeText(MainActivity.this, String.valueOf(answer), Toast.LENGTH_SHORT).show();
+            float answer=0;
+            answer = number1+number2;
+            tvResult.setText(String.valueOf(answer));
+            Log.d("addition",String.valueOf(answer));
+            Toast.makeText(MainActivity.this, String.valueOf(answer) +"addition", Toast.LENGTH_SHORT).show();
 
         }
         if(subtraction){
             float answer = number1-number2;
-            result.setText(answer+"");
+            tvResult.setText(answer+"");
             Toast.makeText(MainActivity.this, String.valueOf(answer), Toast.LENGTH_SHORT).show();
         }
         if(multiplication){
             float answer = number1*number2;
-            result.setText(answer+"");
+            tvResult.setText(answer+"");
             Toast.makeText(MainActivity.this, String.valueOf(answer), Toast.LENGTH_SHORT).show();
         }
         if(division){
             //will raise exception when number2 is equal to 0
-            if((int)number2!=0){
+            if(number2!=0){
                 float answer = number1 / number2;
-                result.setText(answer + "");
+                tvResult.setText(answer + "");
                 Toast.makeText(MainActivity.this, String.valueOf(answer), Toast.LENGTH_SHORT).show();
             }
             else{
@@ -95,81 +98,99 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn0:
-                result.setText(result.getText().toString()+"0");
+            case R.id.button0:
+                tvResult.setText(tvResult.getText().toString()+"0");
                 break;
-            case R.id.btn1:
-                result.setText(result.getText().toString()+"1");
+            case R.id.button1:
+                tvResult.setText(tvResult.getText().toString()+"1");
                 break;
-            case R.id.btn2:
-                result.setText(result.getText().toString()+"2");
+            case R.id.button2:
+                tvResult.setText(tvResult.getText().toString()+"2");
                 break;
-            case R.id.btn3:
-                result.setText(result.getText().toString()+"3");
+            case R.id.button3:
+                tvResult.setText(tvResult.getText().toString()+"3");
                 break;
-            case R.id.btn4:
-                result.setText(result.getText().toString()+"4");
+            case R.id.button4:
+                tvResult.setText(tvResult.getText().toString()+"4");
                 break;
-            case R.id.btn5:
-                result.setText(result.getText().toString()+"5");
+            case R.id.button5:
+                tvResult.setText(tvResult.getText().toString()+"5");
                 break;
-            case R.id.btn6:
-                result.setText(result.getText().toString()+"6");
+            case R.id.button6:
+                tvResult.setText(tvResult.getText().toString()+"6");
                 break;
-            case R.id.btn7:
-                result.setText(result.getText().toString()+"7");
+            case R.id.button7:
+                tvResult.setText(tvResult.getText().toString()+"7");
                 break;
-            case R.id.btn8:
-                result.setText(result.getText().toString()+"8");
+            case R.id.button8:
+                tvResult.setText(tvResult.getText().toString()+"8");
                 break;
-            case R.id.btn9:
-                result.setText(result.getText().toString()+"9");
+            case R.id.button9:
+                tvResult.setText(tvResult.getText().toString()+"9");
                 break;
-            case R.id.btn_ac:
-                result.setText("");
+            case R.id.button_ac:
+                tvResult.setText("");
                 break;
-            case R.id.btn_add:
-                if(result==null){
-                    result.setText("");
+            case R.id.button_add:
+                if(tvResult==null ){
+                    tvResult.setText("");
                 }
                 else {
-                    number1 = Float.parseFloat(result.getText() + "");
+                    number1 = Float.parseFloat(tvResult.getText() + "");
                     add = true;
-                    result.setText(null);
+                    tvResult.setText(null);
+                    buttonAdd.setClickable(false);
+                    buttonSubtract.setClickable(false);
+                    buttonDivide.setClickable(false);
+                    buttonMultiply.setClickable(false);
                 }
                 break;
-            case R.id.btn_sub:
-                if(result==null){
-                    result.setText("");
-                }
-                else {
-                    number1 = Float.parseFloat(result.getText() + "");
+            case R.id.button_subtract:
+                    number1 = Float.parseFloat(tvResult.getText() + "");
                     subtraction = true;
-                    result.setText(null);
-                }
+                    tvResult.setText(null);
+                    buttonAdd.setClickable(false);
+                    buttonSubtract.setClickable(false);
+                    buttonDivide.setClickable(false);
+                    buttonMultiply.setClickable(false);
+
                 break;
-            case R.id.btn_mul:
-                if(result==null){
-                    result.setText("");
-                }
-                else {
-                    number1 = Float.parseFloat(result.getText() + "");
-                    multiplication = true;
-                    result.setText(null);
-                }
+            case R.id.button_multiply:
+                number1 = Float.parseFloat(tvResult.getText() + "");
+                multiplication = true;
+                tvResult.setText("");
+                buttonAdd.setClickable(false);
+                buttonSubtract.setClickable(false);
+                buttonDivide.setClickable(false);
+                buttonMultiply.setClickable(false);
                 break;
-            case R.id.btn_div:
-                if(result==null){
-                    result.setText("");
-                }
-                else {
-                    number1 = Float.parseFloat(result.getText() + "");
+            case R.id.button_divide:
+                //if(tvResult==null){
+                 //   tvResult.setText("");
+                //}
+                //else {
+                    number1 = Float.parseFloat(tvResult.getText() + "");
                     division = true;
-                    result.setText(null);
-                }
+                    tvResult.setText(null);
+                    buttonAdd.setClickable(false);
+                    buttonSubtract.setClickable(false);
+                    buttonDivide.setClickable(false);
+                    buttonMultiply.setClickable(false);
+                //}
                 break;
-            case R.id.btn_eql:
-                number2 = Float.parseFloat(result.getText()+"");
+            case R.id.button_equal:
+                if(add || division || multiplication || subtraction ) {
+                    if(tvResult.getText() != "") {
+                        number2 = Float.parseFloat(tvResult.getText() + "");
+                        buttonAdd.setClickable(true);
+                        buttonSubtract.setClickable(true);
+                        buttonDivide.setClickable(true);
+                        buttonMultiply.setClickable(true);
+                    }
+                    else {
+                        Toast.makeText(this, "Please enter the second number", Toast.LENGTH_SHORT).show();
+                    }
+                }
                 if(add){
                     compute(number1,number2);
                     add = false;
